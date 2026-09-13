717a4a0c35304de0ce04c9a38d5287227bcaebf5
eKisNonos <ekisanon@proton.me>
2026-09-13 16:30:31 +0200

    zk and manifests: no lint switch, no em-dashes

--- src/crypto/zk/nonos_zk/mod.rs
+++ src/crypto/zk/nonos_zk/mod.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-#![allow(clippy::result_unit_err)]
 
 mod attestation;
 mod commit;
